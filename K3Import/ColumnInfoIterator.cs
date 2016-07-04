478690e7e00804f0d6417c933a4b5b81996f6802
sldgoer <13500288851@163.com>
2016-07-04 17:29:11 +0800

    Some modify

---
+++ K3Import/ColumnInfoIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K3Import
+{
+    public class ColumnInfoIterator:IEnumerable<ColunmInfo>
+    {
+        List<ColunmInfo> columns = new List<ColunmInfo>();
+        public void Add(ColunmInfo columninfo)
+        {
+            columns.Add(columninfo);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+
+        public IEnumerator<ColunmInfo> GetEnumerator()
+        {
+            foreach (ColunmInfo ci in columns)
+            {
+                yield return ci;
+            }
+        }
+
+        public ColunmInfo GetColumnInfoByKey(string key)
+        {
+            foreach (ColunmInfo ci in columns)
+            {
+                if (ci.ColumnKey == key)
+                {
+                    return ci;
+                }
+            }
+            return null;
+        }
+
+    }
+}
--- K3Import/ReadK3XML.cs
+++ K3Import/ReadK3XML.cs
@@ -5,70 +5,115 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Xml;
-using LxbXml;
 
 namespace K3Import
 {
     public class ReadK3XML
     {
+        public ColumnInfoIterator ciiterator;
+
         private string _xmlfilename;
         public ReadK3XML(string filename) 
         {
             _xmlfilename = filename;
         }
 
-        public DataTable ReadToTable()
+        public DataTable ReadToTable(string businessname)
         {
-            try
-            {
-                //var columnNames = XMLHelper.GetXmlNodeListByXpath(_xmlfilename,"//");
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            
-            return null;
-        }
-        private void ReadToTable(out DataTable table)
-        {
-            table = new DataTable();
-            //table
-        }
-        private DataRow prepairTableRow()
-        {
-            return null;
-        }
-        private DataRow prepairTableRow()
-        {
-            //DataRowBuilder drb = new DataRowBuilder();            
-            DataRow dr = new DataRow();
+            //DataTable table = ReadToTable(businessname, delegate
+            //{
 
+            //});
+            //table.TableName = businessname;
+            return ReadToTable(businessname, delegate {
+                return GetXmlNodeListByXpath(_xmlfilename, "//xml//rs:data//rs:insert//z:row");
+            });
+                      
+        }
+        private DataTable ReadToTable(string businessname, Func<XmlNodeList> provider)
+        {            
+            using (XmlNodeList xmlnodelist = provider())
+            {
+                DataTable dt = PrepairTableColumn();
+                //String[] columnvalue = new String[dt.Columns.Count]();
+                //int limit = 100;
 
+                foreach (XmlNode xmlnode in xmlnodelist)
+                {
+                    if (xmlnode.Attributes["c6"].Value.IndexOf(businessname) > 0)
+                    {
+                        DataRow dr = dt.NewRow();
+                        foreach (ColunmInfo ci in ciiterator)
+                        {
+                            dr[ci.ColumnName] = xmlnode.Attributes[ci.ColumnKey] == null ? "" : xmlnode.Attributes[ci.ColumnKey].Value;
+                        }
+                        dt.Rows.Add(dr);
+                        //limit--;
+                    }
+                    //if (limit == 0) { break; }
+                }
+                return dt;
+            }
+            //return null;
         }
 
         private DataTable PrepairTableColumn()
         {
             return PrepairTableColumn(_xmlfilename);
-        } 
+        }
         private DataTable PrepairTableColumn(string xmlfilename)
         {
-            //DataTable dt = new DataTable();
-            //datacol
             DataTable dt = new DataTable();
-            var nodelist = XMLHelper.GetXmlNodeListByXpath(xmlfilename, "//s:Schema//s:ElementType");
-            //DataColumn[] colunms = new DataColumn()[nodelist.Count];
-            foreach (XmlNode node in nodelist)
+            ciiterator = GetTableColumnInfo(xmlfilename, delegate {
+                return GetXmlNodeListByXpath(xmlfilename, "//xml//s:Schema//s:ElementType//s:AttributeType");
+            });
+
+            foreach (ColunmInfo ci in ciiterator)
             {
-                if (node.Name == "s:AttributeType")
-                {
-                    dt.Columns.Add(node["name"].Value + node["rs:name"].Value);
-                    //dt.Columns.a
-                }
+                dt.Columns.Add(ci.ColumnName);
             }
             return dt;
         }
-        
-        //private DataTable
+        private ColumnInfoIterator GetTableColumnInfo(string xmlfilename,Func<XmlNodeList> provider)
+        {
+            //DataTable dt = new DataTable();
+            //datacol
+            //DataTable dt = new DataTable();
+            //using (var nodelist = XMLHelper.GetXmlNodeListByXpath(xmlfilename, "//xml//s:Schema//s:ElementType"))
+            using (var nodelist=provider())
+            {
+                //DataColumn[] colunms = new DataColumn()[nodelist.Count];
+                ColumnInfoIterator cii = new ColumnInfoIterator();
+                foreach (XmlNode node in nodelist)
+                {
+                    if (node.LocalName == "AttributeType")
+                    {
+                        cii.Add(new ColunmInfo { ColumnKey = node.Attributes["name"].Value, ColumnName = node.Attributes["rs:name"].Value });
+                        //dt.Columns.Add(node["name"].Value + node["rs:name"].Value);
+                        //dt.Columns.a
+                    }
+                }
+                return cii;
+            }
+           // return dt;
+        }
+
+        private XmlNodeList GetXmlNodeListByXpath(string filename, string xpath)
+        {
+            XmlDocument xd = new XmlDocument();
+            xd.Load(filename);
+
+            XmlNamespaceManager xmlns = new XmlNamespaceManager(xd.NameTable);            
+
+            XmlNode xn =xd.SelectSingleNode("xml");
+
+            foreach (XmlAttribute arr in xn.Attributes)
+            {
+                xmlns.AddNamespace(arr.LocalName, arr.Value);
+            }
+
+
+            return xn.SelectNodes(xpath, xmlns);
+        }
     }
 }
